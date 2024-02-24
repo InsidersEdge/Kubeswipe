c6a5b55433d3c540613dda42b167ad2b4d8643c9
sadath-12 <sadathsadu2002@gmail.com>
2024-02-24 17:06:43 +0530

    support: failed,succeeded pods
    Signed-off-by: sadath-12 <sadathsadu2002@gmail.com>

--- pkg/utils/util.go
+++ pkg/utils/util.go
@@ -7,6 +7,7 @@ import (
 
 	v1 "kubefit.com/kubeswipe/api/v1"
 	"kubefit.com/kubeswipe/pkg/utils/namespaces"
+	"kubefit.com/kubeswipe/pkg/utils/pods"
 	"kubefit.com/kubeswipe/pkg/utils/services"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -14,18 +15,76 @@ import (
 func HandleALLUnusedResources(ctx context.Context, client client.Client, cleaner v1.ResourceCleaner) error {
 	logger := log.FromContext(ctx)
 	if len(cleaner.Spec.Resources.Include) == 0 && len(cleaner.Spec.Resources.Exclude) == 0 {
-		err := namespaces.ForceDeleteTerminatingNamespaces(ctx, client)
+		err := CleanAllResources(ctx, client, cleaner)
 		if err != nil {
-			logger.Error(err, "force deleting namespaces")
-			return err
-		}
-		// if resource there then fetch and monitor them and apply logic
-		err = services.HandleALLUnusedServices(ctx, client, cleaner)
-		if err != nil {
-			logger.Error(err, "handling services")
+			logger.Error(err, "cleaning all resources")
 			return err
 		}
 
+	} else {
+		resourcesMap := make(map[string]bool)
+		for _, includedResource := range cleaner.Spec.Resources.Include {
+			resourcesMap[includedResource.Name] = true
+		}
+		for _, excludedResource := range cleaner.Spec.Resources.Exclude {
+			resourcesMap[excludedResource.Name] = false
+		}
+
+		err := HandleUnusedResourcesInSteps(ctx, client, cleaner, resourcesMap)
+		if err != nil {
+			logger.Error(err, "handling unused resources")
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func HandleUnusedResourcesInSteps(ctx context.Context, client client.Client, cleaner v1.ResourceCleaner, resourceMap map[string]bool) error {
+	logger := log.FromContext(ctx)
+	for resourceName, included := range resourceMap {
+		if resourceName == "Namespace" && included {
+			err := namespaces.ForceDeleteTerminatingNamespaces(ctx, client)
+			if err != nil {
+				logger.Error(err, "force deleting namespaces")
+				return err
+			}
+		}
+		if resourceName == "Service" && included {
+			err := services.HandleALLUnusedServices(ctx, client, cleaner)
+			if err != nil {
+				logger.Error(err, "handling services")
+				return err
+			}
+		}
+		if resourceName == "Pod" && included {
+			err := pods.DeleteAllPendingAndFailedPods(ctx, client)
+			if err != nil {
+				logger.Error(err, "handling services")
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func CleanAllResources(ctx context.Context, client client.Client, cleaner v1.ResourceCleaner) error {
+	logger := log.FromContext(ctx)
+	err := namespaces.ForceDeleteTerminatingNamespaces(ctx, client)
+	if err != nil {
+		logger.Error(err, "force deleting namespaces")
+		return err
+	}
+	err = services.HandleALLUnusedServices(ctx, client, cleaner)
+	if err != nil {
+		logger.Error(err, "handling services")
+		return err
+	}
+	err = pods.DeleteAllPendingAndFailedPods(ctx, client)
+	if err != nil {
+		logger.Error(err, "handling services")
+		return err
 	}
 	return nil
 }
